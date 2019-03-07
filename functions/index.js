4b13eb3996b0df83f4b859f6642626ac822f148c
Sam Kauffman <samvnkauffman@gmail.com>
2019-03-06 21:32:58 -0500

    add responses to intents

--- functions/index.js
+++ functions/index.js
@@ -4,19 +4,33 @@ const functions = require('firebase-functions');
 const app = dialogflow({ debug: true });
 
 /** **** UTILS ***** */
+const randomPop = (arr) => arr[Math.floor(Math.random() * arr.length)];
 const randomNumberFromRange = (min, max) => Math.floor(Math.random() * (max - min + 1) + min);
 
 /** **** DIALOGFLOW ***** */
-app.intent('Default Welcome Intent', (conv) => {
-    conv.ask(`Welcome to your live studio audience! Ready?`);
-});
+// app.intent('Default Welcome Intent', (conv) => {
+//     conv.ask(`Welcome to your live studio audience! Ready?`);
+// });
 
-app.intent('Default Fallback Intent', (conv) => {
-    const randomIndex = randomNumberFromRange(1, 10);
+app.intent(['Default Welcome Intent', 'laugh'], (conv) => {
+    const randomIndex = randomNumberFromRange(1, 17);
+
+    const laughText = randomPop([
+        `HAAHAAHAAHAAHAA`,
+        `HaaHaaHaaHaaHaa`,
+        `HaHaHaHaHaHaHaHa`,
+        `Hahahahahahahaha`,
+        `HAAHAAHAAHAAhahahaha`,
+        `HAHAHAHAHahahaha`,
+    ]);
 
     conv.ask(new SimpleResponse({
-        speech: `<speak><audio src='https://evil-laugh.firebaseapp.com/audio/laugh-${randomIndex}' /></speak>`,
-        text: `HAAHAAHAAHAAHAA`,
+        speech: '<speak></speak>',
+        text: 'Ready?',
+    }));
+    conv.ask(new SimpleResponse({
+        speech: `<speak><audio src='https://evil-laugh.firebaseapp.com/audio/laugh-${randomIndex}.mp3' /></speak>`,
+        text: laughText,
     }));
 });
 
